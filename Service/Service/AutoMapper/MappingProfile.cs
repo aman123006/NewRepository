97490803a22fc9612b2ca00522eeae914fe09c17
Amandeep <aman.01298@gmail.com>
2018-04-02 02:32:24 +0530

    commit

--- Service/Service/AutoMapper/MappingProfile.cs
+++ Service/Service/AutoMapper/MappingProfile.cs
@@ -2,10 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using AutoMapper;
+using DTO;
 
 namespace Service.AutoMapper
 {
     public class MappingProfile
     {
+        
+            public MappingProfile()
+            {
+
+
+            }
+
+         
+
+            public static MapperConfiguration Initialize()
+            {
+                MapperConfiguration config = new MapperConfiguration(cfg => {
+
+                    cfg.CreateMap<CustomerDTO, Customer>();
+                    cfg.CreateMap<Customer, CustomerDTO>();
+                });
+
+                return config;
+            }
+
+
+        
     }
 }
